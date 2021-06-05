d290a520cb63366d9f1f59dbdcc21d0dd2dcf823
piraba <piraba.nagkeeran@tigerspike.com>
2021-06-05 20:15:44 +0800

    Initial Commit

---
+++ CarTrackRepo/CarTrackRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTrackRepoApp.swift
+//  CarTrackRepo
+//
+//  Created by Piraba Nagkeeran on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CarTrackRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
